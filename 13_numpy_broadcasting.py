bd0892855b3f6528dd1e631d0c5d5a6d35fa79c9
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-06-09 22:58:28 +0200

    Update 13_numpy_broadcasting.py

--- 13_numpy_broadcasting.py
+++ 13_numpy_broadcasting.py
@@ -6,3 +6,19 @@
 
 # Dependency
 import numpy as np
+
+# Matching rows length:
+identity_matrix = np.identity(3)
+print(identity_matrix)
+
+single_row_matrix = [0, 0, 1]
+
+result = identity_matrix + single_row_matrix
+print(result)
+
+# Not matching row length:
+identity_matrix = np.identity(3)
+coefficient = 2
+
+result = identity_matrix * coefficient
+print(result)
