3ded4705cfed2402798b1674fdf025bdfa6a8782
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-06-09 23:08:30 +0200

    Update 13_numpy_broadcasting.py

--- 13_numpy_broadcasting.py
+++ 13_numpy_broadcasting.py
@@ -7,7 +7,14 @@
 # Dependency
 import numpy as np
 
-# Matching rows length:
+# CASE 1: Not matching row length:
+identity_matrix = np.identity(3)
+coefficient = 2
+
+result = identity_matrix * coefficient
+print(result)
+
+# CASE 2: Matching rows length:
 identity_matrix = np.identity(3)
 print(identity_matrix)
 
@@ -16,9 +23,14 @@ single_row_matrix = [0, 0, 1]
 result = identity_matrix + single_row_matrix
 print(result)
 
-# Not matching row length:
-identity_matrix = np.identity(3)
-coefficient = 2
+# CASE 3: If the shapes are incompatible, an exception is raised.
+matrix_a = np.array([4, 9, 2, 4])
+matrix_b = np.array([6, 1, 7])
 
-result = identity_matrix * coefficient
-print(result)
+result = matrix_a[:, np.newaxis] + matrix_b
+print(result) # Desired output
+
+try: # Raises a ValueError.
+    result = matrix_a + matrix_b
+except ValueError as err:
+    print(err) # operands could not be broadcast together with shapes (4,) (3,)
