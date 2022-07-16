2b6f14f3651ca3f65443c0b0634fab1d69b8f164
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-17 00:22:42 +0200

    Update 15_numpy_ellipsis_notation.py

--- 15_numpy_ellipsis_notation.py
+++ 15_numpy_ellipsis_notation.py
@@ -9,20 +9,24 @@ import numpy as np
 
 # Data
 matrices = np.arange(27).reshape((3, 3, 3))
+matrices_ones = np.ones((3, 3))
 print(matrices)
 
 # Change all columns of all matrices, if 3 columns then indeces are 0, 1, 2
 matrices = np.arange(27).reshape((3, 3, 3))
+matrices_ones = np.ones((3, 3))
 matrices[:, :, 0] = matrices_ones
 print(matrices)
 
 # Change all rows of all matrices, if 3 rows then indeces are 0, 1, 2
 matrices = np.arange(27).reshape((3, 3, 3))
+matrices_ones = np.ones((3, 3))
 matrices[:, 0, :] = matrices_ones
 print(matrices)
 
 # Change selected matrix, if 3 x 3 matrices then indeces are: 0, 1, 2
 matrices = np.arange(27).reshape((3, 3, 3))
+matrices_ones = np.ones((3, 3))
 matrices[0, :, :] = matrices_ones # First matix: 0
 print(matrices)
 
