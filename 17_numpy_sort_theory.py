8269e2d675b8bed1c6b6f6841b6302c76d54eeb4
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-04 12:37:10 +0200

    Update 17_numpy_sort_theory.py

--- 17_numpy_sort_theory.py
+++ 17_numpy_sort_theory.py
@@ -3,3 +3,37 @@
 # Python 3.9.5
 
 # 17_numpy_sort_theory.py
+
+# Dependency
+import numpy as np
+
+a = np.array([1, 6, 3, 2, 8, 4, 9, 10, 7, 5])
+
+# Sort an unsorted array with np.sort(a)
+print(np.sort(a))
+
+# Get the original indices in their new sorted order
+# by using np.argsort(a)
+print(np.argsort(a))
+
+#   Axis 1
+#   -------->
+# | 3   6   2
+# | 6   1   5
+# v 9   0   1
+# 
+# Axis 0
+
+# Sort Axis 0:
+#   3   0   1
+#   6   1   2
+#   9   6   5
+
+# Sort Axis 1
+#   2   3   6
+#   1   5   6
+#   0   1   9
+
+# The array has two axes: axis 0 (rows) and axis 1 (columns)
+# Vertical sorting takes place along axis 0
+# Horizontal sorting takes place along axis 1
